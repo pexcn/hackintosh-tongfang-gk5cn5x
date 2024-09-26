3042742be6d0647f3cf35e24928aadbda8192d98
pexcn <pexcn97@gmail.com>
2024-09-26 14:22:34 +0800

    add voltageshift for unlock PL1/PL2

---
+++ extras/voltageshift/enable.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p /usr/local/bin/
+curl https://github.com/pexcn/hackintosh-tongfang-gk5cn5x/raw/master/extras/voltageshift/voltageshift \
+  -L -O --output-dir /usr/local/bin/
+chmod +x /usr/local/bin/voltageshift
+
+mkdir -p /Library/LaunchDaemons/
+curl https://github.com/pexcn/hackintosh-tongfang-gk5cn5x/raw/master/extras/voltageshift/com.sicreative.VoltageShift.plist \
+  -L -O --output-dir /Library/LaunchDaemons/
+
+launchctl bootstrap system /Library/LaunchDaemons/com.sicreative.VoltageShift.plist
